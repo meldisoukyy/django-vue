81654567aa43934d5efbd6a8931a9d901a18bddc
Disouky <meldisoukyy@gmail.com>
2024-03-08 23:01:04 +0200

    Update vite configurations

--- frontend/hello/vite.config.ts
+++ frontend/hello/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: process.env.NODE_ENV === "production" ? "/static/frontend/" : "/",
+  build: {
+    outDir: "../../static/frontend",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name].js",
+        entryFileNames: "js/[name].js",
+        assetFileNames: "css/[name].css",
+      },
+    },
+  },
+});
